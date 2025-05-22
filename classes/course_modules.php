239b35b913bfd50a9fda5fe0c94eef1b10b74ef7
jfederico <jesus@123it.ca>
2025-05-22 17:04:33 +0000

    updates to fix dependencies

--- classes/course_modules.php
+++ classes/course_modules.php
@@ -1,11 +1,33 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Course modules class
+ *
+ * @package     block_learningassist
+ * @copyright   2025
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 
 namespace block_learningassist;
 
 use stdClass;
+use moodle_exception;
 
-class course_modules
-{
+class course_modules {
     /**
      * Get course modules
      * @param $course_id
@@ -204,7 +226,11 @@ class course_modules
     {
         // Load html to markdown converter
         global $CFG;
-        require_once($CFG->dirroot . '/blocks/learningassist/classes/html2markdown/vendor/autoload.php');
+        require_once($CFG->dirroot . '/blocks/learningassist/vendor/autoload.php');
+
+        if (!class_exists('\League\HTMLToMarkdown\HtmlConverter')) {
+            throw new \moodle_exception('HtmlConverter class not found. Please ensure html2markdown is installed via Composer.');
+        }
         $converter = new \League\HTMLToMarkdown\HtmlConverter();
 
         //Create object
